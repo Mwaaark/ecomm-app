df72d14f20f41bb89f0b3084a3aa7be67d0797e6
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-04-02 19:27:10 +0800

    initial commit

---
+++ routes/admin/validators.js
@@ -0,0 +1,67 @@
+const { body } = require("express-validator");
+const usersRepo = require("../../repositories/users");
+
+module.exports = {
+  requireTitle: body("title")
+    .trim()
+    .isLength({ min: 5, max: 40 })
+    .withMessage("Must be between 5 and 40 characters"),
+  requirePrice: body("price")
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage("Must be a number greater than 1"),
+  requireEmail: body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Must be a valid email")
+    .custom(async (email) => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error("Email already in use");
+      }
+    }),
+  requirePassword: body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Must be between 4 and 20 characters"),
+  requirePasswordConfirmation: body("passwordConfirmation")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Must be between 4 and 20 characters")
+    .custom((passwordConfirmation, { req }) => {
+      if (passwordConfirmation !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+
+      return true;
+    }),
+  requireEmailExists: body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Must be a valid email")
+    .custom(async (email) => {
+      const user = await usersRepo.getOneBy({ email });
+      if (!user) {
+        throw new Error("Email not found");
+      }
+    }),
+  requireValidPasswordForUser: body("password")
+    .trim()
+    .custom(async (password, { req }) => {
+      const user = await usersRepo.getOneBy({ email: req.body.email });
+      if (!user) {
+        throw new Error("Invalid password");
+      }
+
+      const validPassword = await usersRepo.comparePasswords(
+        user.password,
+        password
+      );
+      if (!validPassword) {
+        throw new Error("Invalid password");
+      }
+    }),
+};
